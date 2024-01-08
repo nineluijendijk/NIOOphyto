e7e3010f13839e28faffac9ea90eac6c3a6aca7e
nineluijendijk <nineluijendijk@gmail.com>
2024-01-08 15:39:57 +0100

    work in progress

--- scripts/PCA.R
+++ scripts/PCA.R
@@ -7,21 +7,28 @@
 library(tidyverse)
 library(here)
 library(pheatmap)
+library(plotly)
 
 load(file = here("data/data_tidy.RData")) # load object data_tidy
-data_tidy <- mutate(data_tidy, LakeType = case_when(Lake == "CL" | Lake == "CH" ~ "Connected",
-                                                    Lake == "TP" | Lake == "CN" | Lake == "MP" ~ "Not connected",
+data_tidy <- mutate(data_tidy, LakeType = case_when(Lake == "CL" | Lake == "CH" ~ "Not connected",
+                                                    Lake == "TP" | Lake == "CN" | Lake == "MP" ~ "Connected",
                                                     Lake == "BP" ~ "Half connected"))
 data_tidy <- filter(data_tidy, Year < 2009)
 data_tidy <- filter(data_tidy, Species != "A. quadrangularis") %>% filter(Species != "Diaptomus")
-data_wide <- pivot_wider(data = data_tidy, names_from = "Species", values_from = "Counts")
+data_extra <- data_tidy %>% mutate("Abundance_ind/L" = (Counts / Proportion_of_sample_counted) / Sampling_volume_net_L,
+                         "Relative_abundance" = Counts / Total_individuals * 100)
+
+data_wide <- pivot_wider(data = select(data_extra, -Counts, -"Relative_abundance"), names_from = "Species", values_from = "Abundance_ind/L")
 data_wide$Combined <- paste(data_wide$Lake, data_wide$Date)
+data_wide <- na.omit(data_wide)
 
-data <- as.data.frame(data_wide[13:35])
+data <- as.data.frame(data_wide[14:36])
 rownames(data) <- data$Combined
-data_matrix <- as.matrix(data[2:23])
+data_matrix <- as.matrix(data[2:12])
 
-pca <- prcomp(data_matrix, scale = TRUE)
+test <- scale(data_matrix)
+
+pca <- prcomp(data_matrix, scale = TRUE) # test here, set scale to false
 pca_summary <- summary(pca)$importance
 
 pca_summary_mutated <- pca_summary %>% t() %>% as_tibble() %>%
@@ -42,8 +49,8 @@ pca_summary_mutated %>% ggplot(aes(x = reorder(PC, -perc_variation), y = perc_va
        x = "PC")+
   geom_text(aes(label = round(perc_variation, digits = 1)), vjust=-0.25)+
   theme_minimal()+
-  theme(legend.position = "none", axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
-  coord_cartesian(ylim = c(0, 15))
+  theme(legend.position = "none", axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))#+
+  #coord_cartesian(ylim = c(0, 15))
 
 pca_plotting <- cbind(data_wide, pca$x)
 
@@ -52,12 +59,17 @@ PC1_var <- round(pca_summary["Proportion of Variance", "PC1"]*100, digits = 1)
 PC2_var <- round(pca_summary["Proportion of Variance", "PC2"]*100, digits = 1)
 
 # Plot PC1 vs PC2
-ggplot(data = pca_plotting, aes(x=PC1, y=PC2, color = LakeType, shape = Lake))+ 
+speciesnames <- colnames(pca_plotting[14:32])
+b <- pivot_longer(data = pca_plotting, cols = all_of(speciesnames), names_to = "Species", values_to = "Value") # so that species can be added to the plot
+
+h <- ggplot(data = b, aes(x=PC1, y=PC2, color = LakeType, shape = Lake))+ 
   geom_point(size = 3, alpha = 0.7)+
   ggtitle("Principal component analysis plot")+
   xlab(paste0("PC1 (", PC1_var, "%)"))+
   ylab(paste0("PC2 (", PC2_var, "%)"))+
   theme_minimal()
+
+ggplotly(h)
 
 data_cor <- as.matrix(cor(data_matrix, method="spearman"))
 pheatmap(data_cor, scale = "row")
