9e19fd6829391c64dc91449575b2515e3ff4fa37
nineluijendijk <nineluijendijk@gmail.com>
2024-02-16 12:10:05 +0100

    preparing data for RDA using exact dates

--- dataRDA.R
+++ dataRDA.R
@@ -1,22 +1,19 @@
 library(tidyverse)
 library(here)
+library(readxl)
 library(writexl)
-library(lubridate)
 
-BP <- read_csv(here("data_raw/Selected_variables_BP.csv"))
-connected <- read_csv(here("data_raw/Selected_variables_connected.csv"))
+overlap <- read.delim(here("data/overlap.txt")) %>% unlist()
 
-BPtidy <- separate(BP, `lake;month;year;ocsed`, into = c("lake", "month", "year", "ocsed"), sep = ";")
-connectedtidy <- separate(connected, `lake;month;year;ocsed`, into = c("lake", "month", "year", "ocsed"), sep = ";")
+data <- read_excel(here("data_raw/OCsed_all_lakes.xlsx"))
 
-data <- rbind(BPtidy, connectedtidy)
+data$Combined <- paste(data$Lake, data$Date)
 
-data$Combined <- paste(data$lake, data$month, data$year)
-data <- filter(data, year < 2009)
+data <- select(data, `OC sed. rate (g C m-2 d-1)`, Combined)
 
-data <- select(data, ocsed, Combined)
+dataF <- filter(data, Combined %in% overlap)
 
-write_xlsx(data, here("data/dataRDA.xlsx"))
+write_xlsx(dataF, here("data/dataRDAOverlap.xlsx"))
 
 
 load(file = here("data/data_tidy.RData")) # load object data_tidy
@@ -30,19 +27,17 @@ data_tidy <- filter(data_tidy, Year > 2005)
 species_to_exclude <- c("Eurycercus lamellatus", "Ostracod", "A. quadrangularis", "Diaptomus")
 data_tidy <- filter(data_tidy, Species %out% species_to_exclude)
 
-lakes_to_exclude <- c("CL","CH")
-data_tidy <- filter(data_tidy, Lake %out% lakes_to_exclude)
-data_tidy$month <- month(data_tidy$Date, label = FALSE) # Add month names
-
-
 data_extra <- data_tidy %>% mutate("Abundance_ind/L" = (Counts / Proportion_of_sample_counted) / Sampling_volume_net_L,
                                    "Relative_abundance" = Counts / Total_individuals * 100)
 
 data_wide <- pivot_wider(data = select(data_extra, -Counts, -"Relative_abundance"), names_from = "Species", values_from = "Abundance_ind/L")
-data_wide$Combined <- paste(data_wide$Lake, data_wide$month, data_wide$Year)
+
+data_wide$Combined <- paste(data_wide$Lake, data_wide$Date)
+
 data_wide <- na.omit(data_wide)
 
-data <- as.data.frame(data_wide[15:35])
+data2 <- as.data.frame(data_wide[14:34])
 
-write_xlsx(data, here("data/data_matrixRDA.xlsx"))
+data2F <- filter(data2, Combined %in% overlap)
 
+write_xlsx(data2F, here("data/data_matrixRDAOverlap.xlsx"))
